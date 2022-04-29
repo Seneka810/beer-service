9b56a914866290af4d8d3af38ea1198e4b0f32aa
Stas Kordyk <stas.kordyk@gmail.com>
2022-04-29 17:29:31 +0300

    refactor dependencies

--- src/test/java/guru/kord/beerservice/BeerServiceApplicationTests.java
+++ src/test/java/guru/kord/beerservice/BeerServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package guru.kord.beerservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BeerServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package guru.kord.beerservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BeerServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
--- src/test/java/guru/kord/beerservice/service/inventory/BeerInventoryServiceRestTemplateImplTest.java
+++ src/test/java/guru/kord/beerservice/service/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -1,27 +1,27 @@
-package guru.kord.beerservice.service.inventory;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@Disabled// for manual testing
-@SpringBootTest
-class BeerInventoryServiceRestTemplateImplTest {
-
-    @Autowired
-    BeerInventoryService beerInventoryService;
-
-    @BeforeEach
-    void setUp() {
-
-    }
-
-    @Test
-    void getOnHandInventory() {
-//        Integer qoh = beerInventoryService.getOnhandInventory(BeerLoader.BEER_1_UUID);
+//package guru.kord.beerservice.service.inventory;
 //
-//        System.out.println(qoh);
-    }
-}
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@Disabled// for manual testing
+//@SpringBootTest
+//class BeerInventoryServiceRestTemplateImplTest {
+//
+//    @Autowired
+//    BeerInventoryService beerInventoryService;
+//
+//    @BeforeEach
+//    void setUp() {
+//
+//    }
+//
+//    @Test
+//    void getOnHandInventory() {
+////        Integer qoh = beerInventoryService.getOnhandInventory(BeerLoader.BEER_1_UUID);
+////
+////        System.out.println(qoh);
+//    }
+//}
